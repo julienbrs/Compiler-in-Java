e759d5fc632e82dabe1450fd4dd3a78f508db025
Reniaud Felix <Felix.Reniaud@grenoble-inp.org>
2023-01-07 10:25:01 +0100

    Ajout d'une joli bannière
    Modification pour une joli bannière

--- src/main/java/fr/ensimag/deca/DecacMain.java
+++ src/main/java/fr/ensimag/deca/DecacMain.java
@@ -12,6 +12,51 @@ import org.apache.log4j.Logger;
 public class DecacMain {
     private static Logger LOG = Logger.getLogger(DecacMain.class);
     
+    public static String setBgColor(int r, int g, int b) {
+        return "\u001b[48;2;"+r+";"+g+";"+b+"m";
+    }
+
+    public static String setFgColor(int r, int g, int b) {
+        return "\u001b[38;2;"+r+";"+g+";"+b+"m";
+    }
+
+    public static String resetFgColor() {
+        return "\u001b[39m";
+    }
+
+    public static String resetBgColor() {
+        return "\u001b[49m";
+    }
+
+    public static void printBanner() {
+        String banner = "╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗\n║            ███████████                    ███           █████         █████████  █████                                    ║\n║           ░░███░░░░░███                  ░░░           ░░███         ███░░░░░███░░███                                     ║\n║            ░███    ░███████████  ██████  █████ ██████  ███████      ███     ░░░  ░███                                     ║\n║            ░██████████░░███░░██████░░███░░███ ███░░███░░░███░      ░███          ░███                                     ║\n║            ░███░░░░░░  ░███ ░░░░███ ░███ ░███░███████   ░███       ░███    █████ ░███                                     ║\n║            ░███        ░███    ░███ ░███ ░███░███░░░    ░███ ███   ░░███  ░░███  ░███      █                              ║\n║            █████       █████   ░░██████  ░███░░██████   ░░█████     ░░█████████  ███████████                              ║\n║           ░░░░░       ░░░░░     ░░░░░░   ░███ ░░░░░░     ░░░░░       ░░░░░░░░░  ░░░░░░░░░░░                               ║\n║                                      ███ ░███                                                                             ║\n║    ██████████                        ░░██████  █████████                                     ███ ████                     ║\n║   ░░███░░░░███                        ░░░░░░  ███░░░░░███                                   ░░░ ░░███                     ║\n║    ░███   ░░███  ██████  ██████  ██████      ███     ░░░   ██████  █████████████  ████████  ████ ░███   ██████  ████████  ║\n║    ░███    ░███ ███░░██████░░███░░░░░███    ░███          ███░░███░░███░░███░░███░░███░░███░░███ ░███  ███░░███░░███░░███ ║\n║    ░███    ░███░███████░███ ░░░  ███████    ░███         ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███████  ░███ ░░░  ║\n║    ░███    ███ ░███░░░ ░███  ██████░░███    ░░███     ███░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███░░░   ░███      ║\n║    ██████████  ░░██████░░██████░░████████    ░░█████████ ░░██████  █████░███ █████░███████  ██████████░░██████  █████     ║\n║   ░░░░░░░░░░    ░░░░░░  ░░░░░░  ░░░░░░░░      ░░░░░░░░░   ░░░░░░  ░░░░░ ░░░ ░░░░░ ░███░░░  ░░░░░░░░░░  ░░░░░░  ░░░░░      ║\n║                                 █████████                        ████     ████    ░███                                    ║\n║                                ███░░░░░███                      ░░███    ░░███    █████                                   ║\n║                               ███     ░░░ ████████  ████████     ░███     ░███   ░░░░░                                    ║\n║                              ░███        ░░███░░███░░███░░███    ░███     ░███                                            ║\n║                              ░███    █████░███ ░░░  ░███ ░███    ░███     ░███                                            ║\n║                              ░░███  ░░███ ░███      ░███ ░███    ░███     ░███                                            ║\n║                               ░░█████████ █████     ░███████     █████    █████                                           ║\n║                                ░░░░░░░░░ ░░░░░      ░███░░░     ░░░░░    ░░░░░                                            ║\n║                                                     ░███                                                                  ║\n║                                                     █████                                                                 ║\n║                                                    ░░░░░                                                                  ║\n╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝";
+        
+        // █ : box   char
+        // ░ : det   char
+        //   : space char
+
+        String textColor = setFgColor(128, 128, 128);
+        String shadowColor = setFgColor(128, 255, 128);
+        String borderColor = setFgColor(128, 255, 128);
+        String backgroundColor = setBgColor(60, 60, 180);
+
+        String box = textColor + "█" + resetFgColor();
+        String dotBox = shadowColor + "█" + resetFgColor();
+        String border = borderColor + "║" + resetFgColor();
+
+        String colorBanner = backgroundColor + banner + resetBgColor();
+        colorBanner = colorBanner.replace("█", box);
+        colorBanner = colorBanner.replace("░", dotBox);
+        colorBanner = colorBanner.replace("╔", borderColor + "╔");
+        colorBanner = colorBanner.replace("╗", "╗" + resetFgColor());
+        colorBanner = colorBanner.replace("╚", borderColor + "╚");
+        colorBanner = colorBanner.replace("╝", "╝" + resetFgColor());
+        colorBanner = colorBanner.replace("║", border);
+        colorBanner = colorBanner.replace("\n", resetBgColor() + "\n" + backgroundColor);
+
+        System.out.println("\n" + colorBanner + "\n");
+    }
+
     public static void main(String[] args) {
         // example log4j message.
         LOG.info("Decac compiler started");
@@ -26,7 +71,8 @@ public class DecacMain {
             System.exit(1);
         }
         if (options.getPrintBanner()) {
-            System.out.println(" ----------GL11----------");
+            // System.out.println(" ----------GL11----------");
+            printBanner();
             System.exit(1);
            // throw new UnsupportedOperationException("decac -b not yet implemented");
         }
