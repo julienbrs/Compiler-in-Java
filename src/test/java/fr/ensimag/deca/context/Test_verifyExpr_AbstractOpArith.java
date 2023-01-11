8f29eaa32c072ea69847f5d41406873eb4d4aa1e
notascii <kearzzcodeur@gmail.com>
2023-01-11 14:56:29 +0100

    test: verifyExpr AbstractOpArith

--- src/test/java/fr/ensimag/deca/context/Test_verifyExpr_AbstractOpArith.java
+++ src/test/java/fr/ensimag/deca/context/Test_verifyExpr_AbstractOpArith.java
@@ -1,13 +1,45 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
 
 /**
  * 
  */
 
-public class Test_verifyExpr_AbstractOpArith.java {
-	@Test
-	public void test() {
-		// TESTTODO
+public class Test_verifyExpr_AbstractOpArith {
+	final Type INT = new IntType(null);
+    final Type FLOAT = new FloatType(null);
+
+	@Mock
+	AbstractExpr lOpInt;
+	@Mock
+	AbstractExpr rOpInt;
+	@Mock
+	AbstractExpr lOpFloat;
+	@Mock
+	AbstractExpr rOpFloat;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		DecacCompiler compiler = DecacCompiler(null, null);
+		when(lOpInt.verifyExpr(compiler, null, null)).thenReturn(INT);
+		when(rOpInt.verifyExpr(compiler, null, null)).thenReturn(INT);
+		when(lOpFloat.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+		when(rOpFloat.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
 	}
+
+	@Test
+	public void testIntInt() {
+		Plus p = new Plus(lOpInt, rOpInt);
+		assertTrue(p.verifyExpr(compiler, null, null).isInt());
+		verify(lOpInt).verifyExpr(compiler, null, null);
+		verify(rOpInt).verifyExpr(compiler, null, null);
+		verify(lOpInt, times(1)).verifyExpr(compiler, null, null);
+		verify(rOpInt, times(1)).verifyExpr(compiler, null, null);
+	}
+
 }
