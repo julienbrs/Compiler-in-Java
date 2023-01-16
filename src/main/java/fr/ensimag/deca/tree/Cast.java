a3bac439e958c8e69aaa3e35e53d060a36f0d726
Reniaud Felix <Felix.Reniaud@grenoble-inp.org>
2023-01-16 17:27:59 +0100

    Encore des classes - ETAPE B

--- src/main/java/fr/ensimag/deca/tree/Cast.java
+++ src/main/java/fr/ensimag/deca/tree/Cast.java
@@ -4,6 +4,7 @@ import java.io.PrintStream;
 
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ClassType;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
@@ -29,11 +30,37 @@ public class Cast extends AbstractExpr {
             // ERROR MSG
             throw new ContextualError("t1 non void : rule 3.??", getLocation());
         }
-        else if (!t.sameType(compiler.environmentType.FLOAT) || !t.sameType(compiler.environmentType.INT)) {
-
+        
+        if (t.sameType(e)) {
+            setType(t);
+            return getType();
+        } else if (t.isInt() && e.isFloat()) {
+            setType(t);
+            return getType();
+        } else if ((t.isFloat() && e.isInt())) {
+            setType(t);
+            return getType();
+        } else if (t.isNull() && e.isClass()) {
+            setType(t);
+            return getType();
+        } else if (t.isClass() && e.isNull()) {
+            setType(t);
+            return getType();
+        } else {
+            // ERROR MSG
+            ClassType tClass = t.asClassType(" rule 3.??", getLocation());
+            // ERROR MSG
+            ClassType eClass = e.asClassType(" rule 3.??", getLocation());
+            if (tClass.isSubClassOf(eClass)) {
+                setType(t);
+                return getType();
+            } else if (eClass.isSubClassOf(tClass)) {
+                setType(t);
+                return getType();
+            }
         }
-        setType(t);
-        return getType();
+        // ERROR MSG
+        throw new ContextualError(" rule 3.??", getLocation());
     }
 
     @Override
---
+++ src/main/java/fr/ensimag/deca/tree/MethodCall.java
@@ -0,0 +1,61 @@
+package fr.ensimag.deca.tree;
+
+import java.io.PrintStream;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ClassType;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.ExpDefinition;
+import fr.ensimag.deca.context.MethodDefinition;
+import fr.ensimag.deca.context.Signature;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+
+/**
+ * Instruction
+ *
+ * @author gl11
+ * @date 16/01/2023
+ */
+public class MethodCall extends AbstractExpr {
+
+    private AbstractExpr expr;
+    private AbstractIdentifier methodIdent;
+    private ListExpr rValStar;
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
+            throws ContextualError {
+        Type e = expr.verifyExpr(compiler, localEnv, currentClass);
+        
+        return getType();
+    }
+
+    @Override
+    protected int codeGenExpr(DecacCompiler compiler, int offset) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+}
