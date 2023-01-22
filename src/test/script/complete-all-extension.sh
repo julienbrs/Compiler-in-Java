47574f88b1e8e7938d35c3c9177825e3924baf3e
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2023-01-22 22:30:16 +0100

    Scripts de tests pour l'extension
    Scripts qui relancent juste tous les tests avec l'option de compil tab
    Il n'y a pas encore les tests spécifiques à l'extension TAB

---
+++ src/test/script/complete-all-extension.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Auteur : gl11
+
+# Script de test de la lexicographie.
+# On lance tous les tests en rapport avec la lexicographie
+# Todo: se servir du log_activated pour echo ou non ?
+
+purple=$(tput setab 99)
+reset=$(tput sgr0)
+
+# On se place dans le répertoire du projet (quel que soit le
+# répertoire d'où est lancé le script) :
+cd "$(dirname "$0")"/../../.. || exit 1
+
+PATH=./src/test/script/launchers:"$PATH"
+
+src/test/script/complete-lexer-extension.sh
+src/test/script/complete-parser-extension.sh
+src/test/script/complete-context-extension.sh
+src/test/script/complete-codegen-extension.sh
