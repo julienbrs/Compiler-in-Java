96bdf056a0f63cb3a0d04ade4d7efca338d22b4e
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2023-01-23 15:39:41 +0100

    Last little changes, go merging

--- src/test/script/provided/basic-lex.sh
+++ src/test/script/provided/basic-lex.sh
@@ -21,9 +21,8 @@ PATH=./src/test/script/launchers:"$PATH"
 # test_lex peut au choix afficher les messages sur la sortie standard
 # (1) ou sortie d'erreur (2). On redirige la sortie d'erreur sur la
 # sortie standard pour accepter les deux (2>&1)
-if test_lex src/test/deca/syntax/lexer/provided/simple_lex.deca 2>&1 \
-    | head -n 1 | grep -q 'simple_lex.deca:[0-9]'
-then
+if test_lex src/test/deca/syntax/lexer/provided/simple_lex.deca 2>&1 |
+    head -n 1 | grep -q 'simple_lex.deca:[0-9]'; then
     echo "Echec inattendu de test_lex"
     exit 1
 else
@@ -31,12 +30,10 @@ else
 fi
 
 # Ligne 10 codée en dur. Il faudrait stocker ça quelque part ...
-if test_lex src/test/deca/syntax/lexer/provided/chaine_incomplete.deca 2>&1 \
-    | grep -q -e 'chaine_incomplete.deca:10:'
-then
+if test_lex src/test/deca/syntax/lexer/provided/chaine_incomplete.deca 2>&1 |
+    grep -q -e 'chaine_incomplete.deca:10:'; then
     echo "Echec attendu pour test_lex"
 else
     echo "Erreur non detectee par test_lex pour chaine_incomplete.deca"
     exit 1
 fi
-
